d05126cbc7d61e2c6f6d6e4c74de10a8700485ad
Satan-Inc <36431133+Satan-Inc@users.noreply.github.com>
2018-02-15 23:22:16 -0500

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float moveSpeed;
+    private Rigidbody2D myRigidbody;
+
+    private bool playerMoving;
+    private Vector2 lastMove;
+    // Use this for initialization
+    void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        playerMoving = false;
+
+if (Input.GetAxisRaw("Horizontal") > 0.1f || Input.GetAxisRaw("Horizontal") < -0.1f)
+        {
+            // transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
+            playerMoving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+
+        }
+
+ if (Input.GetAxisRaw("Vertical") > 0.1f || Input.GetAxisRaw("Vertical") < -0.1f)
+            {
+            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+            playerMoving = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+
+
+        }
+    if(Input.GetAxisRaw("Horizontal") < 0.1f && Input.GetAxisRaw("Horizontal") > -0.1f)
+        {
+            myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y);
+        }
+        if (Input.GetAxisRaw("Vertical") < 0.1f && Input.GetAxisRaw("Vertical") > -0.1f)
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
+        }
+    }
+}
---
+++ damageEnemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damageEnemy : MonoBehaviour {
+    public int damageToGive;
+    public GameObject bloodSplatter;
+    public Transform hitPoint;
+    public GameObject damageNumber;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            //Destroy(other.gameObject);
+            other.gameObject.GetComponent<enemyHealth>().HurtEnemy(damageToGive);
+
+            Instantiate(bloodSplatter, hitPoint.position, hitPoint.rotation);
+
+           var clone = Instantiate(damageNumber, hitPoint.position, Quaternion.Euler(Vector3.zero)) as GameObject;
+
+            clone.GetComponent<DamageNumbers>().damageNumber = damageToGive;
+        }
+    }
+
+}
